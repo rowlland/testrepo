96c38ab07a0538124a95250aafc65924e7e093e3
rowlland <119633952+rowlland@users.noreply.github.com>
2022-12-01 16:30:37 -0600

    Add files via upload
    uploading a new file

---
+++ test 1.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import numpy
+import pandas as pd
+
+df = pd. DataFrame([[10, 20, 30], [40, 50, 60]])
+ser = pd. Series([70, 80, 90], name='new row')
+print('{}\n'. format(df))
+
